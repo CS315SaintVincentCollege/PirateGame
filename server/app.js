c96bc1a383598df4cd4438491541e00e9ab26011
Nate Fabian <natefabian18@gmail.com>
2021-11-23 13:25:13 -0500

    setup socket connections for both server and client + fixed some style and typescript issues

---
+++ server/app.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require('fs'), http = require('http');
+var ws_1 = require("ws");
+var path = require('path');
+var WebSocketPort = 3002;
+var WebServerPort = 3001;
+//#region Web Socket Setttings
+var wss = new ws_1.WebSocketServer({ port: WebSocketPort });
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+    ws.send(JSON.stringify({ type: "Debug", Data: "We Read you Loud and clear" }));
+});
+//#endregion
+//#region Express Server Settup
+http.createServer(function (req, res) {
+    if (req.url == '/') {
+        fs.readFile(__dirname + '/../client/' + 'index.html', function (err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+    }
+    else {
+        fs.readFile(__dirname + '/../client/' + req.url, function (err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+    }
+}).listen(WebServerPort);
+console.log("WemServer @ http://localhost:" + WebServerPort + " and sockets are on port " + WebSocketPort);
+//#endregion
--- server/game.js
+++ server/game.js
@@ -1,3 +1,4 @@
+"use strict";
 var Board = /** @class */ (function () {
     function Board() {
         this.boardSize = 10;
